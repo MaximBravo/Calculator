0ce77ba4ef0c8da9c661cb2716ebc808d2ec1a58
Maxim Bravo <maxim@alexbravo.com>
2017-01-12 08:50:18 -0800

    Voice input works for list of numbers

--- app/src/main/java/com/maximbravo/calculator/MainActivity.java
+++ app/src/main/java/com/maximbravo/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.maximbravo.calculator;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,26 +11,102 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final int RESULT_SPEECH = 1;
+    private TextView textBox;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        listView = (ListView) findViewById(R.id.list);
+        textBox = (TextView) findViewById(R.id.text_box);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            launchVoiceIntent();
             }
         });
     }
 
+    public void launchVoiceIntent(){
+        Intent intent = new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+        try {
+            startActivityForResult(intent, RESULT_SPEECH);
+            textBox.setText("");
+        } catch (ActivityNotFoundException a) {
+            Toast t = Toast.makeText(getApplicationContext(),
+                    "Opps! Your device doesn't support Speech to Text",
+                    Toast.LENGTH_SHORT);
+            t.show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String resultText = text.get(0);
+                    textBox.setText(resultText);
+                    putIntoListView(resultText);
+                }
+                break;
+            }
+
+        }
+    }
+    public void putIntoListView(String resultText){
+        ArrayList<String> stringList = getArrayListFrom(resultText.toLowerCase());
+        if(stringList.size() != 0){
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    stringList );
+
+            listView.setAdapter(arrayAdapter);
+        }
+    }
+    public ArrayList<String> getArrayListFrom(String text){
+        ArrayList<String> numbers = new ArrayList<>();
+
+        String[] seperators = {", ", ",", " space "};
+        for(int i = 0; i < seperators.length; i++) {
+            String currentSeperator = seperators[i];
+            if (text.contains(currentSeperator)) {
+                numbers = convertArrayToArrayList(text.split(currentSeperator));
+                break;
+            }
+        }
+        return numbers;
+    }
+    public ArrayList<String> convertArrayToArrayList(String[] numbers){
+        ArrayList<String> result = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++){
+            result.add(numbers[i]);
+        }
+        return result;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
